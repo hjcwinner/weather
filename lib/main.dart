c3289e493a7bfd6da84180042df0d8926bde2d06
hjcwinner <hjcwinner@naver.com>
2021-02-27 02:31:33 +0900

    날씨 Icon추가 및 검색 실패, 무정보, 내위치 날씨

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'dart:io';
+import 'package:geolocator/geolocator.dart';
 
 void main() {
   runApp(WeatherApp());
@@ -12,22 +14,43 @@ class WeatherApp extends StatefulWidget {
 }
 
 class _WeatherAppState extends State<WeatherApp> {
-  int temperature = 0;
+  int temperature;
   String location = 'seoul';
   int woeid = 1132599;
   String weather = 'clear';
+  String abbrevation = "";
+  String errorMessage = "";
 
-  String searchApiUrl = 'https://www.metaweather.com/api/location/search/?query=';
+  final Geolocator geolocator = Geolocator()..forceAndroidLocationManager;
+
+  Position _currentPosition;
+  String _currentAddress;
+
+  String searchApiUrl =
+      'https://www.metaweather.com/api/location/search/?query=';
   String locationApiUrl = 'https://www.metaweather.com/api/location/';
 
-  void fetchSearch( String input) async{
-    var searchResult = await http.get(searchApiUrl + input);
-    var result = json.decode(searchResult.body)[0];
+  @override
+  void initState() {
+    super.initState();
+    fetchLocation();
+  }
 
-    setState(() {
-      location = result["title"];
-      woeid = result["woeid"];
-    });
+  void fetchSearch(String input) async {
+    try {
+      var searchResult = await http.get(searchApiUrl + input);
+      var result = json.decode(searchResult.body)[0];
+
+      setState(() {
+        location = result["title"];
+        woeid = result["woeid"];
+        errorMessage = "";
+      });
+    } catch (e) {
+      setState(() {
+        errorMessage = "정확한 도시 이름을 입력해주세요.";
+      });
+    }
   }
 
   void fetchLocation() async {
@@ -38,64 +61,141 @@ class _WeatherAppState extends State<WeatherApp> {
 
     setState(() {
       temperature = data['the_temp'].round();
-      weather = data['weather_state_name'].replaceAll(' ','').toLowerCase();
-      print(weather);
+      weather = data['weather_state_name'].replaceAll(' ', '').toLowerCase();
+      abbrevation = data['weather_state_abbr'];
     });
   }
 
-  void onTextFieldSubmitted(String input) {
-    fetchSearch(input);
-    fetchLocation();
+  void onTextFieldSubmitted(String input) async {
+    await fetchSearch(input);
+    await fetchLocation();
+  }
+
+  // void getLocation() async {
+  //   Position position = await Geolocator.getCurrentPosition(
+  //       desiredAccuracy: LocationAccuracy.high);
+  //   print(position);
+  // }
+
+  _getCurrentLocation() {
+    geolocator
+        .getCurrentPosition(desiredAccuracy: LocationAccuracy.best)
+        .then((Position position) {
+      setState(() {
+        _currentPosition = position;
+      });
+
+      _getAddressFromLatLng();
+    }).catchError((e) {
+      print(e);
+    });
+  }
+
+  _getAddressFromLatLng() async {
+    try {
+      List<Placemark> p = await geolocator.placemarkFromCoordinates(
+          _currentPosition.latitude, _currentPosition.longitude);
+
+      Placemark place = p[0];
+
+      setState(() {
+        _currentAddress =
+            "${place.locality}, ${place.postalCode}, ${place.country}";
+      });
+      onTextFieldSubmitted(place.locality);
+      print(place.locality);
+    } catch (e) {
+      print(e);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        debugShowCheckedModeBanner: false,
         home: Container(
-      decoration: BoxDecoration(
-          image: DecorationImage(
-        image: AssetImage('images/$weather.png'),
-        fit: BoxFit.cover,
-      )),
-      child: Scaffold(
-        backgroundColor: Colors.transparent,
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Column(
-              children: [
-                Center(
-                    child: Text(
-                  temperature.toString() + ' ℃',
-                  style: TextStyle(fontSize: 60, color: Colors.white),
-                )),
-                Center(
-                    child: Text(location,
-                        style: TextStyle(fontSize: 40, color: Colors.white)))
-              ],
-            ),
-            Column(
-              children: [
-                Container(
-                  width: 300,
-                  child: TextField(
-                    onSubmitted: (String input){
-                      onTextFieldSubmitted(input);
-                    },
-                    style: TextStyle(color: Colors.white, fontSize: 25),
-                    decoration: InputDecoration(
-                      hintText: '검색하고 싶은 도시를 입력하세요',
-                      hintStyle: TextStyle(color: Colors.white, fontSize: 18),
-                      prefixIcon: Icon(Icons.search, color: Colors.white,)
-                    ),
+          decoration: BoxDecoration(
+              image: DecorationImage(
+            image: AssetImage('images/$weather.png'),
+            fit: BoxFit.cover,
+          )),
+          child: temperature == null
+              ? Center(child: CircularProgressIndicator())
+              : Scaffold(
+                  appBar: AppBar(
+                    actions: [
+                      Padding(
+                        padding: EdgeInsets.only(right: 20),
+                        child: GestureDetector(
+                          onTap: () {
+                            _getCurrentLocation();
+                          },
+                          child: Icon(Icons.location_history, size: 36.0),
+                        ),
+                      ),
+                    ],
+                    backgroundColor: Colors.transparent,
+                    elevation: 0,
                   ),
-                )
-              ],
-            )
-          ],
-        ),
-      ),
-    ));
+                  resizeToAvoidBottomInset: false,
+                  backgroundColor: Colors.transparent,
+                  body: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Column(
+                        children: [
+                          Center(
+                            child: Image.network(
+                              'https://www.metaweather.com/static/img/weather/png/' +
+                                  abbrevation +
+                                  '.png',
+                              width: 100,
+                            ),
+                          ),
+                          Center(
+                              child: Text(
+                            temperature.toString() + ' ℃',
+                            style: TextStyle(fontSize: 60, color: Colors.white),
+                          )),
+                          Center(
+                              child: Text(location,
+                                  style: TextStyle(
+                                      fontSize: 40, color: Colors.white)))
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Container(
+                            width: 300,
+                            child: TextField(
+                              onSubmitted: (String input) {
+                                onTextFieldSubmitted(input);
+                              },
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 25),
+                              decoration: InputDecoration(
+                                  hintText: '검색하고 싶은 도시를 입력하세요',
+                                  hintStyle: TextStyle(
+                                      color: Colors.white, fontSize: 18),
+                                  prefixIcon: Icon(
+                                    Icons.search,
+                                    color: Colors.white,
+                                  )),
+                            ),
+                          ),
+                          Text(
+                            errorMessage,
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                color: Colors.redAccent,
+                                fontSize: Platform.isAndroid ? 15 : 20),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+        ));
   }
 }
